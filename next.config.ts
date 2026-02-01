76fda64dc720b89d68dadccaba5f84d13359685b
jiayutee <93061118+jiayutee@users.noreply.github.com>
2026-02-01 21:32:03 +0000

    revert to previous state

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',  // Enable static export
   typescript: {
     ignoreBuildErrors: true,
   },
